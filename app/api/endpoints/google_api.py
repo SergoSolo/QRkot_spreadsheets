0aca99a55734b225f06d11c547bcc14f44735881
Сергей Соловьев <ser5olovjew@yandex.ru>
2022-12-07 08:37:40 +0300

    fix func google

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -24,10 +24,10 @@ async def get_report(
 ):
     """Только для суперюзеров."""
     projects = await charity_project_crud.get_projects_by_completion_rate(session)
-    spreadsheetid = await spreadsheets_create(wrapper_services)
-    await set_user_permissions(spreadsheetid, wrapper_services)
-    await spreadsheets_update_value(spreadsheetid,
+    spreadsheet_id = await spreadsheets_create(wrapper_services)
+    await set_user_permissions(spreadsheet_id, wrapper_services)
+    await spreadsheets_update_value(spreadsheet_id,
                                     projects,
                                     wrapper_services)
-    logger.info(f'Таблица доступна по адресу: https://docs.google.com/spreadsheets/d/{spreadsheetid}')
+    logger.info(f'Таблица доступна по адресу: https://docs.google.com/spreadsheets/d/{spreadsheet_id}')
     return projects
